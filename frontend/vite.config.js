4996374e722675a8ab2f2c1c7bc3223c611fdaf1
VarghesePJ <vargheseputhuvajoseph@gmail.com>
2025-03-17 15:08:15 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/World-360",
   server: {
     port: 3000,
   }
